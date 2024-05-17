601d35e4d41cac4549db3a0ae7fa859c82a4c69c
Matheus Sanabria <codificadoresdenegocios@gmail.com>
2024-05-17 14:26:51 -0300

    Exemplos práticos com React + Redux

--- src/Counter.js
+++ src/Counter.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import './App.css';
 
 class Counter extends React.Component {
+
   increment = () => {
-    this.props.dispatch({ type: 'INC', payload: 1 });
+    //TODO: Chamar função para incrementar no próprio redux.
+      this.props.dispatch({
+        'type':'INCREMENT',
+        //'title': 'Codificadores de Negócios',
+        //'name':'João'
+      });
   };
 
   changeTitle = () => {
-    this.props.dispatch({ type: 'info', title: 'Novo Título' });
-  };
+    this.props.dispatch({
+      'type':'ALTERAR_TITULO',
+      //'title': 'Codificadores de Negócios',
+      //'name':'João'
+    });
+};
 
   render() {
     return (
       <main className='App'>
-        <h2 className='text-center'>Hello World {this.props.count}</h2>
+        <h2 className='text-center'>Hello World</h2>
         <button onClick={this.increment}>Incrementar!</button>
         <button onClick={this.changeTitle}>Alterar Título!</button>
         <h2>{this.props.count}</h2>
@@ -28,6 +39,6 @@ const mapStateToProps = state => ({
   count: state.count,
   title: state.title,
   name: state.name
-
 });
+
 export default connect(mapStateToProps)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';  // Use createRoot instead of ReactDOM.render
 import './index.css';
+import axios from 'axios'; 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import Counter from './Counter';
@@ -12,10 +13,28 @@ const initialStates = {
 };
 
 function reducer(state = initialStates, action) {
+
+    if(action.type === 'INCREMENT'){
+        return {
+          count:state.count + 1,
+          title:state.title,
+          name:state.name
+        }
+    } else if(action.type === 'ALTERAR_TITULO'){
+      axios.get('https://my-json-server.typicode.com/typicode/demo/posts').then((response)=>{
+        console.log(response);
+        store.dispatch({type:'ALTERAR_TITULO_FINAL',title:response.data[0].title});
+      })
+    } else if(action.type = 'ALTERAR_TITULO_FINAL'){
+      return {count: state.count, title: action.title}
+    }
+
   return state;
 }
 
 const store = createStore(reducer);
+
+
 
 const App = () => (
   <Provider store={store}>
